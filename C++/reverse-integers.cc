43aaa4d1f6bf8383df09575d1de361943afb2911
Tamás Ujvári <tamasujvari@Tamass-MacBook-Air.local>
2023-11-26 15:55:30 +0100

    solution2

---
+++ C++/reverse-integers.cc
@@ -0,0 +1,43 @@
+    #include <iostream>
+    #include <vector>
+    #include <climits>
+
+    using namespace std;
+    class Solution {
+    public:
+        int reverse(int x) {
+            vector<int> digits = getDigits(x);
+            printVector(digits);
+
+            long long reversed = convertVectorToLongLong(digits);
+
+            if(reversed > INT_MAX || reversed < INT_MIN){
+                return 0;
+            }
+            return (int)reversed;
+        }
+    private:
+        long long convertVectorToLongLong(vector<int> v1){
+            long long ret = 0;
+            for(auto i:v1){
+                ret = ret*10 + i;
+            }
+            return ret;
+        }
+
+        vector<int> getDigits(int x){
+            vector<int> digits;
+            while(x!=0){
+                digits.push_back(x%10);
+                x/=10;
+            }
+            return digits;
+        }
+
+        void printVector(vector<int> v1){
+            for(auto i:v1){
+                cout <<i<<" ";
+            }
+            cout << endl;
+        }
+    };
--- desc.md
+++ desc.md
@@ -12,7 +12,8 @@
 |  #  | Title           |  Solution           |  Difficulty    | Note| 
 |-----|---------------- | --------------- | --------------- |-----|
 1 | [Two Sum](https://leetcode.com/problems/two-sum/description/) | [C++](./C++/two-sum.cpp) | Easy         |||
-2 | [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/) | [C++](./C++/add-two-numbers.cc) | Medium
+2 | [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/) | [C++](./C++/add-two-numbers.cc) | Medium         |||
+2 | [Reverse integers](https://leetcode.com/problems/reverse-integer/) | [C++](./C++/reverse-integers.cc) | Medium
 
 ## Python
 |  #  | Title           |  Solution           |  Difficulty    | Note| 
