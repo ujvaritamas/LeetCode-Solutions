6a524d187bcd55a68e01b5d830b9ebffde3d163f
Tamás Ujvári <tamasujvari@Tamass-MacBook-Air.local>
2023-11-30 10:13:47 +0100

    optimalization medianoftwosortedarray

--- C++/medianOfTwoSortedArray.cc
+++ C++/medianOfTwoSortedArray.cc
@@ -1,36 +1,31 @@
 #include <vector>
-#include <math>
+#include <cmath>
 #include <iostream>
 
 using namespace std;
 
 class Solution {
 public:
-    double getMedian(vector<int> &v){
-        auto size = v.size();
-            if(size == 0){
-                return 0;
+    double getMedian(const vector<int> &v, const int& size){
+        if(size == 0){
+            return 0;
+        }
+        else{
+            if(size%2 == 0){
+                return static_cast<double>(v[size/2]+v[size/2-1])/2;
             }
             else{
-                if(size%2 == 0){
-                    return static_cast<double>(v[size/2]+v[size/2-1])/2;
-                }
-                else{
-                    return static_cast<double>(v[size/2]);
-                }
+                return static_cast<double>(v[size/2]);
             }
         }
+    }
 
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> nums3((int)nums1.size() + (int)nums2.size());
+        int merged_size = (int)nums1.size() + (int)nums2.size();
+        vector<int> nums3(merged_size);
 
         merge(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), nums3.begin());
 
-        for (auto it : nums3) {
-            cout << it << " ";
-        }
-
-        cout << endl;
-        return getMedian(nums3);
+        return getMedian(nums3, merged_size);
     }
 };
